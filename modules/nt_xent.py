a4a925ef58658dbfa2d81494696fecc21a51b45a
dawoon-leem <dawoon.leem@navercorp.com>
2020-12-05 22:14:55 +0900

    update:nt_xent loss 변경
    labels가 다 zero 인게 이상해서, 구글 깃헙 코드 보고 고쳐봤습니다.

--- modules/nt_xent.py
+++ modules/nt_xent.py
@@ -12,17 +12,13 @@ class NT_Xent(nn.Module):
         self.device = device
         self.world_size = world_size
 
-        self.mask = self.mask_correlated_samples(batch_size, world_size)
-        self.criterion = nn.CrossEntropyLoss(reduction="sum")
+        self.mask = self.get_mask(batch_size, world_size)
+        self.criterion = nn.CrossEntropyLoss()
         self.similarity_f = nn.CosineSimilarity(dim=2)
 
-    def mask_correlated_samples(self, batch_size, world_size):
-        N = 2 * batch_size * world_size
-        mask = torch.ones((N, N), dtype=bool)
+    def get_mask(self, batch_size, world_size):
+        mask = torch.ones((batch_size, batch_size), dtype=bool)
         mask = mask.fill_diagonal_(0)
-        for i in range(batch_size * world_size):
-            mask[i, batch_size + i] = 0
-            mask[batch_size + i, i] = 0
         return mask
 
     def forward(self, z_i, z_j):
@@ -30,25 +26,16 @@ class NT_Xent(nn.Module):
         We do not sample negative examples explicitly.
         Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
         """
-        N = 2 * self.batch_size * self.world_size
-
-        z = torch.cat((z_i, z_j), dim=0)
-        if self.world_size > 1:
-            z = torch.cat(GatherLayer.apply(z), dim=0)
-
-        sim = self.similarity_f(z.unsqueeze(1), z.unsqueeze(0)) / self.temperature
-
-        sim_i_j = torch.diag(sim, self.batch_size * self.world_size)
-        sim_j_i = torch.diag(sim, -self.batch_size * self.world_size)
-
-        # We have 2N samples, but with Distributed training every GPU gets N examples too, resulting in: 2xNxN
-        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(
-            N, 1
-        )
-        negative_samples = sim[self.mask].reshape(N, -1)
-
-        labels = torch.zeros(N).to(positive_samples.device).long()
-        logits = torch.cat((positive_samples, negative_samples), dim=1)
-        loss = self.criterion(logits, labels)
-        loss /= N
+        sim_ii = self.similarity_f(z_i.unsqueeze(1), z_i.unsqueeze(0)) / self.temperature
+        sim_ii = sim_ii[self.mask].reshape(self.batch_size-1, -1)
+        sim_jj = self.similarity_f(z_j.unsqueeze(1), z_j.unsqueeze(0)) / self.temperature
+        sim_jj = sim_jj[self.mask].reshape(self.batch_size-1, -1)
+        sim_ij = self.similarity_f(z_i.unsqueeze(1), z_j.unsqueeze(0)) / self.temperature
+        sim_ji = self.similarity_f(z_j.unsqueeze(1), z_i.unsqueeze(0)) / self.temperature
+        labels = torch.cat(
+            (torch.arange(1, self.batch_size), torch.zeros(self.batch_size))
+        ).to(sim_ii.device).long()  # torch.Size([256])
+        loss_a = self.criterion(torch.cat((sim_ij, sim_ii), dim=0), labels)
+        loss_b = self.criterion(torch.cat((sim_ji, sim_jj), dim=0), labels)
+        loss = (loss_a + loss_b)/2
         return loss
